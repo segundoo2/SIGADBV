1a1efe99f82e1e89492e76a89dc19e5e0dc771fa
Edilson Segundo <edilsonsilva13@gmail.com>
2026-07-04 21:48:49 -0300

    chore - padroniza config do eslint com outros projetos

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -4,7 +4,7 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config([
+export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
@@ -38,4 +38,4 @@ export default tseslint.config([
       '@typescript-eslint/unbound-method': 'off',
     },
   },
-]);
+);
