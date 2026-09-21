632a59e7b93b2adc95a7282fcd727385fc93046d
Edilson Segundo <edilsonsilvaa13@gmail.com>
2026-09-21 15:55:16 -0300

    refactor - implementa const de baseUrl

--- frontend/src/environments/environments.ts
+++
@@ -1,6 +0,0 @@
-const isProduction = typeof window !== 'undefined' && window.location.hostname !== 'localhost';
-
-export const environment = {
-  production: isProduction,
-  apiUrl: isProduction ? 'https://api.sgcode.com.br' : 'http://localhost:3000'
-};
